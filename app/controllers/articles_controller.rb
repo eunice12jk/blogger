f55aa398329d73886d1e72055484d123ecd9698b
eunice12jk <euchiri@gmail.com>
2017-04-01 14:36:27 +0300

    Adding the delete action

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,10 +1,33 @@
 class ArticlesController < ApplicationController
+  include ArticlesHelper
+
   def index
     @articles = Article.all
   end
-  
+
   def show
   	@article = Article.find(params[:id])
 
   end
+
+  def new
+  	@article = Article.new
+
+  end 
+
+  def create
+    @article = Article.new(params[:article])
+      #(title: params[:article][:title],
+      #body: params[:article][:body])
+    #@article.title = params[:article][:title]
+    @article.save
+    redirect_to article_path(@article)
+
+  def edit
+    @article = Article.find(params[:id])
+  end
+
+
+  end
+
 end
