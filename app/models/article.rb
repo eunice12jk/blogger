7fa42d07c62f9bea075212904c19d53871b195c0
eunice12jk <euchiri@gmail.com>
2017-04-10 18:35:35 +0300

    Added login

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,7 +1,7 @@
 class Article < ActiveRecord::Base
   has_many :comments, dependent: :destroy
 
-  has_many :taggings
+  has_many :taggings, dependent: :destroy
   has_many :tags, through: :taggings
   
   def tag_list
